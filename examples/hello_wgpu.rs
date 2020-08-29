bb35bd332b19385f2ce6a8af3b55bd22a0694390
Jared Stephen <grok_moo@yahoo.com>
2020-08-29 09:29:56 -0700

    WIP compile spirv

--- examples/hello_wgpu.rs
+++ examples/hello_wgpu.rs
@@ -8,7 +8,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let font_src = include_bytes!("data/fonts/Roboto-Medium.ttf");
     let image_src = include_bytes!("data/images/gui-minimal.png");
     let image = image::load_from_memory(image_src).unwrap().to_rgba();
-    let theme_src = include_str!("data/theme-minimal.yml");
+    let theme_src = include_str!("data/theme-empty.yml");
     let theme: serde_yaml::Value = serde_yaml::from_str(theme_src)?;
     let window_size = [1280.0, 720.0];
 
@@ -26,10 +26,10 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut renderer = thyme::WgpuRenderer::new();
     let mut context_builder = thyme::ContextBuilder::new(theme, &mut renderer, &mut io)?;
 
-    // register resources in thyme and create the context
-    let image_dims = image.dimensions();
-    context_builder.register_texture("gui", &image.into_raw(), image_dims)?;
-    context_builder.register_font_source("roboto", font_src.to_vec())?;
+    // TODO register resources in thyme and create the context
+    // let image_dims = image.dimensions();
+    // context_builder.register_texture("gui", &image.into_raw(), image_dims)?;
+    // context_builder.register_font_source("roboto", font_src.to_vec())?;
     let mut context = context_builder.build()?;
 
     // run main loop
--- src/wgpu_backend/mod.rs
+++ src/wgpu_backend/mod.rs
@@ -6,6 +6,8 @@ pub struct WgpuRenderer {
 
 impl WgpuRenderer {
     pub fn new() -> WgpuRenderer {
+        build_spirv();
+
         WgpuRenderer {
             
         }
@@ -32,3 +34,57 @@ impl Renderer for WgpuRenderer {
         todo!()
     }
 }
+
+fn build_spirv() {
+    use shaderc;
+
+    let mut compiler = shaderc::Compiler::new().unwrap();
+    let vert_result = compiler.compile_into_spirv(
+        VERT_SHADER_SRC,
+        shaderc::ShaderKind::Vertex,
+        "vert.glsl",
+        "main",
+        None
+    ).unwrap();
+
+    let frag_result = compiler.compile_into_spirv(
+        FRAGMENT_SHADER_SRC,
+        shaderc::ShaderKind::Fragment,
+        "frag.glsl",
+        "main",
+        None
+    ).unwrap();
+}
+
+const VERT_SHADER_SRC: &str = r#"
+  #version 140
+
+  in vec2 position;
+  in vec2 tex;
+  in vec3 color;
+
+  layout(location = 0) out vec2 v_tex_coords;
+  layout(location = 1) out vec3 v_color;
+
+  void main() {
+    gl_Position = vec4(position, 0.0, 1.0);
+	
+    v_tex_coords = tex;
+    v_color = color;
+  }
+"#;
+
+const FRAGMENT_SHADER_SRC: &str = r#"
+  #version 140
+
+  layout(location = 0) in vec2 v_tex_coords;
+  layout(location = 1) in vec3 v_color;
+
+  out vec4 color;
+
+  uniform sampler2D tex;
+
+  void main() {
+    color = vec4(v_color, 1.0) * texture(tex, v_tex_coords);
+  }
+"#;
