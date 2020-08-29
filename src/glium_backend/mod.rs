e6207e169924f83ebf385cbe2e43c6a2b6bb07bf
Jared Stephen <grok_moo@yahoo.com>
2020-08-28 17:35:22 -0700

    use texture border clamp

--- src/glium_backend/mod.rs
+++ src/glium_backend/mod.rs
@@ -273,9 +273,9 @@ impl Renderer for GliumRenderer {
             minify_filter: MinifySamplerFilter::Linear,
             magnify_filter: MagnifySamplerFilter::Nearest,
             wrap_function: (
-                SamplerWrapFunction::Repeat,
-                SamplerWrapFunction::Repeat,
-                SamplerWrapFunction::Repeat,
+                SamplerWrapFunction::Clamp,
+                SamplerWrapFunction::Clamp,
+                SamplerWrapFunction::Clamp,
             ),
             ..Default::default()
         };
