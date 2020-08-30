bdefdeeeba2ca20d5041a2dc3fee854d3499e7e8
Jared Stephen <grok_moo@yahoo.com>
2020-08-29 22:17:03 -0700

    compile shaders successfully

--- src/wgpu_backend/mod.rs
+++ src/wgpu_backend/mod.rs
@@ -57,17 +57,21 @@ fn build_spirv() {
 }
 
 const VERT_SHADER_SRC: &str = r#"
-  #version 140
+  #version 420
 
-  in vec2 position;
-  in vec2 tex;
-  in vec3 color;
+  layout(set = 0, binding = 0) uniform View {
+      mat4 matrix;
+  };
+
+  layout(location = 0) in vec2 position;
+  layout(location = 1) in vec2 tex;
+  layout(location = 2) in vec3 color;
 
   layout(location = 0) out vec2 v_tex_coords;
   layout(location = 1) out vec3 v_color;
 
   void main() {
-    gl_Position = vec4(position, 0.0, 1.0);
+    gl_Position = matrix * vec4(position, 0.0, 1.0);
 	
     v_tex_coords = tex;
     v_color = color;
@@ -75,14 +79,14 @@ const VERT_SHADER_SRC: &str = r#"
 "#;
 
 const FRAGMENT_SHADER_SRC: &str = r#"
-  #version 140
+  #version 420
+
+  layout(set = 1, binding = 0) uniform sampler2D tex;
 
   layout(location = 0) in vec2 v_tex_coords;
   layout(location = 1) in vec3 v_color;
 
-  out vec4 color;
-
-  uniform sampler2D tex;
+  layout(location = 0) out vec4 color;
 
   void main() {
     color = vec4(v_color, 1.0) * texture(tex, v_tex_coords);
