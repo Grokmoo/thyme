8c781bf77c1abde2705ac6c69b838b81efb6e848
Jared Stephen <grok_moo@yahoo.com>
2021-03-26 22:40:15 -0700

    markdown headings and emphasis/strong support

--- src/text_area.rs
+++ src/text_area.rs
@@ -4,22 +4,37 @@ use crate::{Frame, Rect, Point};
 
 impl Frame {
     /**
-    A text area widget that parses markdown text.
+    A text area widget that parses markdown text.  Child themes need to be defined for each font / size
+    combination that you want to be able to render.
 
     An example theme definition:
     ```yaml
     text_area:
-      # the actual text is rendered on the children so you don't want to specify any font or
-      # layout options here
       background: gui/window_bg_base
       border: { all: 5 }
       size: [0, 150]
       width_from: Parent
-      custom_floats:
-        line_height: 10.0
+      custom:
+        line_height: 14.0
       children:
-        item:
-          from: label
+        paragraph_normal:
+          from: text_area_item
+          font: small
+        paragraph_strong:
+          from: text_area_item
+          font: small_bold
+        paragraph_emphasis:
+          from: text_area_item
+          font: small_italic
+        paragraph_strong_emphasis:
+          from: text_area_item
+          font: small_bold_italic
+        heading1_normal:
+          from: text_area_item
+          font: heading1
+        heading2_normal:
+          from: text_area_item
+          font: heading2
       text: |
         This is multiline text in *YAML* with some
         basic markdown formatting.
@@ -33,19 +48,13 @@ impl Frame {
     ```
     **/
     pub fn text_area(&mut self, theme: &str) {
-        let line_height = self.custom_float(theme, "line_height", 10.0);
-        let normal_font = self.custom_string(theme, "normal_font", String::new());
-        let strong_font = self.custom_string(theme, "strong_font", String::new());
-        let emphasis_font = self.custom_string(theme, "emphasis_font", String::new());
-
         let mut state = MarkdownState {
-            line_height,
-            normal_font,
-            strong_font,
-            emphasis_font,
-            cur_font: FontMode::Normal,
+            line_height: self.custom_float(theme, "line_height", 10.0),
             indent: 0.0,
             cursor: Point::default(),
+            font: FontMode::Normal,
+            size: SizeMode::Paragraph,
+            cur_theme: "paragraph_normal".to_string(),
         };
 
         let builder = self.start(theme);
@@ -64,19 +73,16 @@ impl Frame {
                         state.start_tag(tag);
                     },
                     Event::End(tag) => {
-                        state.end_tag(tag);
+                        state.end_tag(ui, tag);
                     },
                     Event::Text(text) => {
                         item(ui, &mut state, text.to_string());
-                        state.update_cursor(ui);
                     },
                     Event::SoftBreak => {
-                        state.new_line(1.0);
-                        state.update_cursor(ui);
+                        state.new_line(ui, 1.0);
                     },
                     Event::HardBreak => {
-                        state.new_line(2.0);
-                        state.update_cursor(ui);
+                        state.new_line(ui, 2.0);
                     },
                     Event::Rule | Event::Code(_) | Event::Html(_) | Event::FootnoteReference(_) | Event::TaskListMarker(_) => {
                         ui.log(log::Level::Warn, format!("Tag {:?} event is unsupported", event));
@@ -94,32 +100,39 @@ fn item(
 ) {
     let original_y = state.cursor.y;
 
-    ui.start("item")
+    ui.start(state.cur_theme())
         .text(text)
         .text_indent(state.indent)
-        .font(state.cur_font())
         .trigger_text_layout(&mut state.cursor)
         .finish();
     
     state.cursor.y += original_y;
+    state.update_cursor(ui);
 }
 
 struct MarkdownState {
     line_height: f32,
     cursor: Point,
     indent: f32,
-    normal_font: String,
-    strong_font: String,
-    emphasis_font: String,
+    
+    size: SizeMode,
+    font: FontMode,
 
-    cur_font: FontMode,
+    cur_theme: String, // computed based on size and font
 }
 
 impl MarkdownState {
     fn start_tag(&mut self, tag: Tag) {
         match tag {
-            Tag::Paragraph => {}
-            Tag::Heading(_) => {}
+            Tag::Paragraph => self.size = SizeMode::Paragraph,
+            Tag::Heading(level) => {
+                self.set_size(match level {
+                    1 => SizeMode::Heading1,
+                    2 => SizeMode::Heading2,
+                    3 => SizeMode::Heading3,
+                    _ => SizeMode::Paragraph,
+                });
+            },
             Tag::BlockQuote => {}
             Tag::CodeBlock(_) => {}
             Tag::List(_) => {}
@@ -129,18 +142,23 @@ impl MarkdownState {
             Tag::TableHead => {}
             Tag::TableRow => {}
             Tag::TableCell => {}
-            Tag::Emphasis => self.cur_font = FontMode::Emphasis,
-            Tag::Strong => self.cur_font = FontMode::Strong,
+            Tag::Emphasis => self.set_font(self.font.push(FontMode::Emphasis)),
+            Tag::Strong => self.set_font(self.font.push(FontMode::Strong)),
             Tag::Strikethrough => {}
             Tag::Link(_, _, _) => {}
             Tag::Image(_, _, _) => {}
         }
     }
 
-    fn end_tag(&mut self, tag: Tag) {
+    fn end_tag(&mut self, ui: &mut Frame, tag: Tag) {
         match tag {
-            Tag::Paragraph => {}
-            Tag::Heading(_) => {}
+            Tag::Paragraph => {
+                self.new_line(ui, 2.0);
+            },
+            Tag::Heading(_) => {
+                self.set_size(SizeMode::Paragraph);
+                self.new_line(ui, 2.0);
+            },
             Tag::BlockQuote => {}
             Tag::CodeBlock(_) => {}
             Tag::List(_) => {}
@@ -150,17 +168,33 @@ impl MarkdownState {
             Tag::TableHead => {}
             Tag::TableRow => {}
             Tag::TableCell => {}
-            Tag::Emphasis => self.cur_font = FontMode::Normal,
-            Tag::Strong => self.cur_font = FontMode::Normal,
+            Tag::Emphasis => self.set_font(self.font.remove(FontMode::Emphasis)),
+            Tag::Strong => self.set_font(self.font.remove(FontMode::Strong)),
             Tag::Strikethrough => {}
             Tag::Link(_, _, _) => {}
             Tag::Image(_, _, _) => {}
         }
     }
 
-    fn new_line(&mut self, lines: f32) {
+    fn set_font(&mut self, font: FontMode) {
+        self.font = font;
+        self.recompute_theme();
+    }
+
+    fn set_size(&mut self, size: SizeMode) {
+        self.size = size;
+        self.recompute_theme();
+    }
+
+    fn recompute_theme(&mut self) {
+        self.cur_theme = format!("{}_{}", self.size.theme(), self.font.theme());
+    }
+
+    fn new_line(&mut self, ui: &mut Frame, lines: f32) {
         self.cursor.x = 0.0;
+        self.indent = 0.0;
         self.cursor.y += lines * self.line_height;
+        self.update_cursor(ui);
     }
 
     fn update_cursor(&mut self, ui: &mut Frame) {
@@ -168,17 +202,92 @@ impl MarkdownState {
         ui.set_cursor(0.0, self.cursor.y);
     }
 
-    fn cur_font(&self) -> &str {
-        match self.cur_font {
-            FontMode::Normal => &self.normal_font,
-            FontMode::Strong => &self.strong_font,
-            FontMode::Emphasis => &self.emphasis_font,
+    fn cur_theme(&self) -> &str {
+        &self.cur_theme
+    }
+}
+
+#[derive(Copy, Clone)]
+enum SizeMode {
+    Paragraph,
+    Heading1,
+    Heading2,
+    Heading3,
+}
+
+impl SizeMode {
+    fn theme(self) -> &'static str {
+        use SizeMode::*;
+        match self {
+            Paragraph => "paragraph",
+            Heading1 => "heading1",
+            Heading2 => "heading2",
+            Heading3 => "heading3",
         }
     }
 }
 
+#[derive(Copy, Clone)]
 enum FontMode {
     Normal,
     Strong,
     Emphasis,
+    StrongEmphasis,
+}
+
+impl FontMode {
+    fn theme(self) -> &'static str {
+        use FontMode::*;
+        match self {
+            Normal => "normal",
+            Strong => "strong",
+            Emphasis => "emphasis",
+            StrongEmphasis => "strong_emphasis",
+        }
+    }
+
+    fn push(self, other: FontMode) -> FontMode {
+        use FontMode::*;
+        match self {
+            Normal => other,
+            Strong => match other {
+                Normal => Strong,
+                Strong => Strong,
+                Emphasis => StrongEmphasis,
+                StrongEmphasis => StrongEmphasis,
+            },
+            Emphasis => match other {
+                Normal => Emphasis,
+                Strong => StrongEmphasis,
+                Emphasis => Emphasis,
+                StrongEmphasis => StrongEmphasis,
+            },
+            StrongEmphasis => StrongEmphasis,
+        }
+    }
+
+    fn remove(self, other: FontMode) -> FontMode {
+        use FontMode::*;
+        match self {
+            Normal => Normal,
+            Strong => match other {
+                Normal => Strong,
+                Strong => Normal,
+                Emphasis => Strong,
+                StrongEmphasis => Normal,
+            },
+            Emphasis => match other {
+                Normal => Emphasis,
+                Strong => Emphasis,
+                Emphasis => Normal,
+                StrongEmphasis => Normal,
+            },
+            StrongEmphasis => match other {
+                Normal => StrongEmphasis,
+                Strong => Emphasis,
+                Emphasis => Strong,
+                StrongEmphasis => Normal,
+            }
+        }
+    }
 }
